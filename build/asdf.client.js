18c277f527e5ad0cf60c5850146c7a1c1ba25597
k10526 <k10526@nate.com>
2014-06-11 20:34:03 +0900

    chain.js 버그 수정

--- build/asdf.client.js
+++ build/asdf.client.js
@@ -4634,9 +4634,9 @@
     //Asdf.Chain("    asdfasdfasdfasdf").bind(Asdf.S.trim).bind(Asdf.S.capitalize).bind(Asdf.S.truncate, undefined, 5, '...').bind(Asdf.S.lpad, undefined, '0', 10).value();
     function Chain(obj, conf){
         if(obj instanceof Chain) return obj;
-        if(!(this instanceof Chain)) return new Chain(obj)
+        if(!(this instanceof Chain)) return new Chain(obj, conf)
         this._value = obj;
-        this._conf = {};
+        this._conf = conf||{};
         if(conf) {
             conf.bind && (this.bind = conf.bind);
             conf.fail && (this.fail = conf.fail);
